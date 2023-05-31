2716718e71fceaee71b223e19932689aa35a2f3f
fertilian <fertiliast2002@gmail.com>
2023-06-01 00:04:14 +0700

    buy product

---
+++ lib/screens/checkout_screen/buy_product.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Checkout extends StatelessWidget;
+const Checkout ({super.key});
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+  appBar: AppBar(
+  backgroundColor: Colors.white,
+  centerTitle: true,
+title: const Text(
+"Check Out",
+style: TextStyle(
+color: Colors.black
+),
+),
+  ),
+body: Padding(
+padding: const,
+),
+  );
+}
