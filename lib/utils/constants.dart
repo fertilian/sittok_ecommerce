7cd1859272f0357116c87cd04efb87f0a9656471
sindisukma <117698581+sindisukma@users.noreply.github.com>
2023-06-01 10:54:54 +0700

    editwarna

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 // Colors
 const kBackgroundColor = Color(0xFFF9F9FC);
 const kPrimaryColor = Color(0xFF816C57);
-const kSecondaryColor = Color(0xFF4C53A5);
+const kSecondaryColor = Color(0xFFC8B6D5);
 
 const kLightProductColor = Color(0xFFEDEBEA);
 const kDarkProductColor = Color(0xFF998878);
