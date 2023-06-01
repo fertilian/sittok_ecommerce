104962cdd19291cf506055bd0f5765a9cffd4ae4
sindisukma <117698581+sindisukma@users.noreply.github.com>
2023-06-01 10:24:19 +0700

    splashsindi

---
+++ lib/screens/splash_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../../utils/constants.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: kSecondaryColor,
+      body: Center(
+        child: Text(
+          'Logo',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 35,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
