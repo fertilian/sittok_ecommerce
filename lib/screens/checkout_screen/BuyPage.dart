863b4a01dffe38faf0ba063eec795d46a4de185d
fertilian <fertiliast2002@gmail.com>
2023-06-03 12:56:55 +0700

    buy app bar

---
+++ lib/screens/checkout_screen/BuyPage.dart
@@ -0,0 +1,64 @@
+import 'package:ecommerce_ui/widgets/BuyItemSamples.dart';
+import 'package:flutter/material.dart';
+import 'package:ecommerce_ui/widgets/BuyAppBar.dart';
+import 'package:flutter/rendering.dart';
+
+class BuyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          BuyProduct(),
+
+          Container(
+            //t
+            height: 700,
+            padding: EdgeInsets.only(top: 15),
+            decoration: BoxDecoration(
+                color: Color(0xFFEDECF2),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(35),
+                  topRight: Radius.circular(35),
+                )),
+            child: Column(children: [
+              BuyItemSamples(),
+              Container(
+                //decoration: BoxDecoration(
+                //borderRadius: BorderRadius.circular(10),
+                // ),
+                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                padding: EdgeInsets.all(10),
+                child: Row(children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFF4C53A5),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      "Add Coupon Code",
+                      style: TextStyle(
+                        color: Color(0xFF4C53A5),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ],),
+              ),
+            ],
+            ),
+          ),
+        ],
+      ),
+      //bottomNavigationBar: CartBottomNavbBar(),
+    );
+  }
+}
---
+++ lib/widgets/BuyItemSamples.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class BuyItemSamples extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 110,
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10 ),
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(children: [
+            Radio(value: "",
+                groupValue: "",
+               activeColor: Color(0xFF4C53A5),
+            onChanged: (Index){},
+            ),
+            Container(
+              height: 70,
+              width: 70,
+              margin: EdgeInsets.only(right: 15),
+              child: Image.asset("images/cod.png"),
+            ),
+            Padding(padding: EdgeInsets.symmetric(vertical: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Cash On Delivery",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF4C53A5),
+                ),
+                ),
+              ],
+            ),),
+          ],
+          ),
+        ),
+        Container(
+          height: 110,
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10 ),
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(children: [
+            Radio(value: "",
+              groupValue: "",
+              activeColor: Color(0xFF4C53A5),
+              onChanged: (Index){},
+            ),
+            Container(
+              height: 70,
+              width: 70,
+              margin: EdgeInsets.only(right: 15),
+              child: Image.asset("images/cod.png"),
+            ),
+            Padding(padding: EdgeInsets.symmetric(vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Cash On Delivery",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF4C53A5),
+                    ),
+                  ),
+                ],
+              ),),
+          ],
+          ),
+        ),
+      ],
+
+    );
+  }
+}
