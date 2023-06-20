942242a1add7da47626ed344f5ffeb461f2d7a53
sindisukma <117698581+sindisukma@users.noreply.github.com>
2023-06-20 22:52:05 +0700

    sindi push help ya

--- lib/Help/HelpCenter.dart
+++ lib/Help/HelpCenter.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MaterialApp(
   home: HelpCenter(),
@@ -9,7 +10,7 @@ class HelpCenter extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Help Center',
           style: TextStyle(
             fontSize: 15.0,
@@ -22,17 +23,72 @@ class HelpCenter extends StatelessWidget {
         backgroundColor: Colors.purple,
       ),
       body: Center(
-        child: Text(
-          'Selamat datang di Help Center kami.'
-              'Kami di sini untuk membantu Anda menemukan solusi dan jawaban atas pertanyaan dan masalah yang Anda hadapi'
-          'Help Center kami dirancang untuk memberikan panduan yang jelas dan mendalam mengenai produk dan layanan kami.'
-          'Silakan temukan jawaban yang Anda cari dengan menjelajahi berbagai topik yang tersedia.'
-              'Jika Anda tidak menemukan jawaban yang Anda cari di Help Center kami, jangan ragu untuk menghubungi tim dukungan kami. Anda dapat menghubungi kami melalui [0897654321] atau melalui formulir kontak yang tersedia di Help Center.',
-          style: TextStyle(
-            fontSize: 20.0,
-            letterSpacing: 1.0,
-            color: Colors.black,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'Selamat datang di Help Center Sittok.',
+              style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 1.0,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              'Kami di sini untuk membantu Anda menemukan solusi dan jawaban atas pertanyaan dan masalah yang Anda hadapi.',
+              style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 1.0,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              'Help Center kami dirancang untuk memberikan panduan yang jelas dan mendalam mengenai produk dan layanan kami.',
+              style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 1.0,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              'Silakan temukan jawaban yang Anda cari dengan menjelajahi berbagai topik yang tersedia.',
+              style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 1.0,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              'Jika Anda tidak menemukan jawaban yang Anda cari di Help Center kami, jangan ragu untuk menghubungi tim dukungan kami. Anda dapat menghubungi kami melalui WhatsApp di [nomor WhatsApp] atau melalui email dan Instagram.',
+              style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 1.0,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Spacer(),
+            IconButton(
+              icon: Icon(Icons.phone),
+              onPressed: () {
+                launch("https://wa.me/message/XNEWEBRQ5UTSJ1?src=qr");
+              },
+            ),
+            const Spacer(),
+            IconButton(
+              icon: Icon(Icons.link),
+              onPressed: () {
+                launch("https://instagram.com/inisittok?igshid=OGQ5ZDc2ODk2ZA==");
+              },
+            ),
+            const Spacer(),
+          ],
         ),
       ),
     );
